6b7e4dac55dba6961b4b035364e8e0c3e4a2c979
YoavAvisar123 <yoavavisar2@gmail.com>
2024-11-02 20:45:48 +0200

    commit

--- main.py
+++ main.py
@@ -20,20 +20,35 @@ def window():
 
 
 def main_text():
-    font_size = pixels2points(width/10)
+    font_size = pixels2points(width / 10)
     label = Label(root, text="Screen Talk", bg=bg_color, fg='white', font=('ariel', font_size))
-    label.pack(pady=height//20)
+    label.pack(pady=height // 20)
 
 
 def exit_button():
     font_size = pixels2points(width / 40)
-    exitB = Button(root, text="Exit", width=width//300, bg="#DC143C", command=root.destroy, font=("ariel", font_size),
+    exitB = Button(root, text="Exit", width=width // 300, bg="#DC143C", command=root.destroy, font=("ariel", font_size),
                    fg="white", activebackground="#DC143C", activeforeground="white", bd=0, relief=SUNKEN)
-    exitB.pack(pady=(height * 0.5, 0))
+    exitB.pack(pady=(height * 0.25, 0))
+
+
+def login_button():
+    font_size = pixels2points(width / 40)
+    loginB = Button(root, text="LOGIN", width=width // 300, bg="#32CD32", font=("ariel", font_size),
+                    fg="white", activebackground="#32CD32", activeforeground="white", bd=0, relief=SUNKEN)
+    loginB.pack(padx=(0, width // 3), pady=(height // 10, 0))
+
+
+def signup_button():
+    font_size = pixels2points(width / 40)
+    signupB = Button(root, text="LOGIN", width=width // 300, bg="#32CD32", font=("ariel", font_size),
+                     fg="white", activebackground="#32CD32", activeforeground="white", bd=0, relief=SUNKEN)
+    signupB.pack(padx=(0, width // 3), pady=(height // 10, 0))
 
 
 def main_window():
     main_text()
+    login_button()
     exit_button()
 
 
