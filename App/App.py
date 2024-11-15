f5259c344226483f9ab184414d2aae7d566e623b
YoavAvisar123 <yoavavisar2@gmail.com>
2024-11-15 17:22:04 +0200

    commit

--- App/App.py
+++ App/App.py
@@ -2,6 +2,7 @@ import socket
 from home_page import HomePage
 from tkinter import *
 import threading
+from threading import Event
 import sys
 
 class App:
@@ -9,34 +10,15 @@ class App:
         self.host = host
         self.port = port
 
-        self.client = None
-        self.root = None
-
-        threading.Thread(target=self.start_client, daemon=True).start()
-
-        threading.Thread(target=self.start_gui).start()
-
-    def start_gui(self):
-        self.root = Tk()
-        HomePage(self.root)
-        self.root.mainloop()
-        self.client.close()
-        sys.exit()
-
-    def start_client(self):
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.client.connect((self.host, self.port))
 
-        connected = True
-        while connected:
-                msg = input("Enter message to send (or 'exit' to quit): ")
-                if msg.lower() == 'exit':
-                    connected = False
-                    self.client.close()
-                else:
-                    self.client.send(msg.encode("utf-8"))
-                    response = self.client.recv(1024).decode("utf-8")
-                    print(f"[SERVER] {response}")
+        self.root = Tk()
+        HomePage(self.root)
+        self.root.mainloop()
+
+        self.client.close()
+        sys.exit()
 
 
 if __name__ == '__main__':
